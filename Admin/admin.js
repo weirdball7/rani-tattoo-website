3827f84177975015bf9c86b1a077c203abb2d291
weirdball7 <moteamitay04@gmail.com>
2023-11-22 00:08:10 +0200

    Added javascript for user auth
    probably wont use it and switch to a method where i check that the login input(still dont have login page set up) is equal to a predeterment admin credentials.

--- Admin/admin.js
+++ Admin/admin.js
@@ -1,3 +1,41 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+    let userName; 
+    let password; 
+    let passwordRepeat; 
+    let user;
+    let btn = document.getElementById('btn');
+    console.log(user);
+
+    const buttonClick = () => {
+        userName = document.getElementById('user-name').value;
+        password = document.getElementById('password').value;
+        passwordRepeat = document.getElementById('password-repeat').value;
+        userName = userName.toLowerCase();
+        console.log(userName, password, passwordRepeat);
+
+        // if (userName === 'rotem') {
+        //     alert('Sucsses');
+        // }else{
+        //     alert('Error');
+        // };
+
+        if(password.length >= 6){
+            if(password === passwordRepeat){
+                user = {
+                    'userName':userName,
+                    'password':password
+                };
+                console.log(user);
+            }else{
+                alert('passwords Dont match');
+            };
+        }else{
+            alert('password too short');
+        };
+
+
+    };
+
+    btn.addEventListener('click', buttonClick);
+
 });
